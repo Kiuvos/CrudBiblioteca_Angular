ad9370c8fe3d37d395a81e331121146cc1dd68fa
kiuvos <jesus04.jc7@gmail.com>
2023-09-24 11:55:17 -0500

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Libros} from './models/libros'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcrud';
+  LibrosArray:Libros[]=[
+    {id: 1, name:"El principito", autor:"no je",date:"10/10/2001",state:"Disponible" },
+    {id: 2, name:"Don quijote", autor:":v",date:"10/10/2003",state:"Dañado" },
+    {id: 3, name:"Diccionario Español", autor:"añskdj",date:"10/10/2021",state:"Perdido" },
+    {id: 4, name:"Harry potter", autor:"xd",date:"10/10/2005",state:"Prestado" },
+    
+  ];
+binding: any;
+
+  selectedLibros: Libros =new Libros();
+
+  addorEdit(){
+    if(this.selectedLibros.id===0){
+      this.selectedLibros.id=this.LibrosArray.length+1;
+      this.LibrosArray.push(this.selectedLibros);
+    }
+    this.selectedLibros=new Libros();
+  }
+
+  openForEdit(Libros: Libros){
+    this.selectedLibros=Libros;
+  }
+
+  delete(){
+    if (confirm('Are you sure that you want to delete it?')){
+      this.LibrosArray=this.LibrosArray.filter(i=>i!=this.selectedLibros);
+      this.selectedLibros=new Libros;
+    }
+    
+  }
 }
+
---
+++ src/app/models/libros.ts
@@ -0,0 +1,7 @@
+export class Libros {
+    id: number =0;
+    name: string="";
+    autor: string ="";
+    date: string="";
+    state:string="";
+}
